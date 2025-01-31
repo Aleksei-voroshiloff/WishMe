e07ea9c79cb7dce4687d3d0e9649774c82704cb4
Aleksei-voroshiloff <alexey909@gmail.com>
2025-01-31 19:20:40 +0300

    eche luche

--- client/src/2_pages/WishListPage/WishListPage.tsx
+++ client/src/2_pages/WishListPage/WishListPage.tsx
@@ -4,18 +4,29 @@ import style from './WishListPage.module.scss';
 
 export default function WishListPage(): React.JSX.Element {
   const { wishListCards, error, loading } = useAppSelector((state) => state.wishlist);
+  const user = useAppSelector((state) => state.user.data);
 
   if (loading) return <div>Loading...</div>;
   if (error === 'error') return <div>{error}</div>;
   //   const {userid} = useParams()
 
   return (
-    // {user.id === userid ? }
+    // {user.id === userid ? div}
+    <div>
+      <div style={{ display: 'flex', margin: '50px' }}>
+        {user?.id === wishListCards[0]?.userId ? (
+          <>
+            <h1>+</h1>
 
-    <div className={style.razmap}>
-      {wishListCards.map((list) => (
-        <WishListCardUi key={list.id} list={list} />
-      ))}
+            <h1 style={{ marginLeft: '100px' }}>Добавить</h1>
+          </>
+        ) : null}
+      </div>
+      <div className={style.razmap}>
+        {wishListCards.map((list) => (
+          <WishListCardUi key={list.id} list={list} />
+        ))}
+      </div>
     </div>
   );
 }
