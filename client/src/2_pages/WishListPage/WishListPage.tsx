da8403df4ad688dfd75b3d2ab166be16670f5728
kep4ik18 <gummatovkenan@gmail.com>
2025-02-02 21:21:35 +0300

    стиль для подарков и общий стиль страниц

--- client/src/2_pages/WishListPage/WishListPage.tsx
+++ client/src/2_pages/WishListPage/WishListPage.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../1_app/store/hooks';
 import WishListCardUi from '../../5_entities/wishlist/ui/WishListCardUi';
 import style from './WishListPage.module.scss';
@@ -5,29 +6,32 @@ import style from './WishListPage.module.scss';
 export default function WishListPage(): React.JSX.Element {
   const { wishListCards, error, loading } = useAppSelector((state) => state.wishlist);
   const user = useAppSelector((state) => state.user.data);
+  const navigate = useNavigate();
 
   if (loading) return <div>Loading...</div>;
   if (error === 'error') return <div>{error}</div>;
-  //   const {userid} = useParams()
+
+  const handleCardClick = (id: number): void => {
+   void  navigate(`/wishlist/${String(id)}`);
+  };
 
   return (
-    // {user.id === userid ? div}
-    
-    <div>
+    <main className={style.main}>
       <div style={{ display: 'flex', margin: '50px' }}>
         {user?.id === wishListCards[0]?.userId ? (
           <>
             <h1>+</h1>
-
             <h1 style={{ marginLeft: '100px' }}>Добавить</h1>
           </>
         ) : null}
       </div>
       <div className={style.razmap}>
         {wishListCards.map((list) => (
-          <WishListCardUi key={list.id} list={list} />
+          <div key={list.id} onClick={() => handleCardClick(list.id)} >
+            <WishListCardUi list={list} />
+          </div>
         ))}
       </div>
-    </div>
+    </main>
   );
 }
