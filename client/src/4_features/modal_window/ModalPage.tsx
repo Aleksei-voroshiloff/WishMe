de3367439b80dddfd3e9de473a79a868003b8950
Aleksei-voroshiloff <alexey909@gmail.com>
2025-02-03 14:32:04 +0300

    user in modal

--- client/src/4_features/modal_window/ModalPage.tsx
+++ client/src/4_features/modal_window/ModalPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import {
   ModalHeader,
   ModalContent,
@@ -7,33 +7,75 @@ import {
   Modal,
   Form,
   FormField,
-  FormTextArea,
+  Image,
 } from 'semantic-ui-react';
+import { useAppDispatch, useAppSelector } from '../../1_app/store/hooks';
+import { closeUserModal } from '../../5_entities/modal_window/model/modalSlice';
+import style from './Modal.module.scss';
 
 export default function ModalPage(): React.JSX.Element {
+  const dispatch = useAppDispatch();
+  const showUserModal = useAppSelector((state) => state.modal.showUserModal);
+  const data = useAppSelector((state) => state.user.data)
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [avatar, setAvatar] = useState<string | null>(data?.avatar ?? null);
+
+  const handleImageClick = (): void => {
+    fileInputRef.current?.click(); // Открываем диалог выбора файла
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setAvatar(reader.result as string); // Обновляем аватар
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    // <Modal size="small" open={} onClose={() => dispatch(closeModal())}>
-    //   <ModalHeader>Редакция заметки</ModalHeader>
-    //   <ModalContent>
-    //     <Form>
-    //       <FormField>
-    //         <label>Название</label>
-    //         <input name="title" type="text" defaultValue={note?.title} />
-    //       </FormField>
-    //       <FormField>
-    //         <label>История</label>
-    //         <FormTextArea name="text" type="text" defaultValue={note?.text} />
-    //       </FormField>
-    //       <ModalActions style={{ marginTop: '20px' }}>
-    //         <Button negative onClick={() => dispatch(closeModal())}>
-    //           Вернуться
-    //         </Button>
-    //         <Button positive type="submit">
-    //           Добавить
-    //         </Button>
-    //       </ModalActions>
-    //     </Form>
-    //   </ModalContent>
-    // </Modal>
+    <Modal size="small" open={showUserModal} onClose={() => dispatch(closeUserModal())}>
+      <ModalHeader>Личный кабинет</ModalHeader>
+      <ModalContent>
+        <Form>
+          <div>
+            <Image
+              htmlFor="upload"
+              onClick={handleImageClick} // Обработчик клика
+              style={{ cursor: 'pointer' }}
+              className={style.avatar}
+              src={avatar ?? '/HomerSimpson.webp'}
+            />
+            <input
+              ref={fileInputRef}
+              accept="image/*"
+              onChange={handleFileChange}
+              id="upload"
+              className={style.addFoto}
+              type="file"
+              name="file"
+            />
+          </div>
+          <FormField>
+            <label>Имя пользователя</label>
+            <input name="name" type="text" defaultValue={data?.name} />
+          </FormField>
+          <FormField>
+            <label>Номер телефона</label>
+            <input name="phoneNumber" type="text" defaultValue={data?.phoneNumber} />
+          </FormField>
+          <FormField>
+            <label>Ваша дата рождения</label>
+            <input name="birthday" type="date" defaultValue={data?.birthday} />
+          </FormField>
+          <ModalActions style={{ marginTop: '20px' }}>
+            <Button onClick={() => dispatch(closeUserModal())}>Вернуться</Button>
+            <Button type="submit">Сохранить</Button>
+          </ModalActions>
+        </Form>
+      </ModalContent>
+    </Modal>
   );
 }
--- client/src/5_entities/modal_window/model/modalSlice.ts
+++ client/src/5_entities/modal_window/model/modalSlice.ts
@@ -1,3 +1,25 @@
-// import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-// type ModalState = {};
+type ModalState = {
+  showUserModal: boolean;
+};
+
+const initialState: ModalState = {
+  showUserModal: false,
+};
+
+const modalSlice = createSlice({
+  name: 'Modal',
+  initialState,
+  reducers: {
+    openUserModal(state) {
+      state.showUserModal = true;
+    },
+    closeUserModal(state) {
+      state.showUserModal = false;
+    },
+  },
+});
+
+export const { openUserModal, closeUserModal } = modalSlice.actions;
+export default modalSlice.reducer;
